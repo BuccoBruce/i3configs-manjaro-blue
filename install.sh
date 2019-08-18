7d93523e90893755bf739b9b79efbbd08d287060
Chris McMillan <dcmcmillan@gmail.com>
2019-08-18 12:03:33 -0700

    Correct dir structure, set wallpaper, automate file copy

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ sudo pacman-mirrors --country United_States
 sudo pacman -Syu 
 sudo pacman -S --noconfirm chromium timeset dbus-python polybar feh xorg-xbacklight intel-media-driver git ttf-font-awesome python-pip code jq
 sudo yay -S spotify libva-intel-driver-g45-h264 moonlight ttf-weather-icons
+feh --bg-scale wallpapers/wallhaven-1315q3.jpg
+rsync -av dotfiles/.* ~/
