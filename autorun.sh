4291dc4676d36af280cbfb33f204b8a6df18e4d7
Chris McMillan <dcmcmillan@gmail.com>
2019-08-18 10:01:07 -0700

    Fixed autoinstall.sh

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo pacman -Syy
+sudo sudo pacman -Syy
 sudo pacman -S pacman-mirrors
-sudo pacman-mirrors --fasttrack && sudo pacman -Syyu
-sudo pacman -Syu pamac
+sudo pacman-mirrors --country United_States 
+sudo pacman -Syu 
+sudo pacman -S --noconfirm chromium timeset dbus-python polybar feh xorg-xbacklight intel-media-driver git ttf-font-awesome python-pip code jq
+sudo yay -S spotify libva-intel-driver-g45-h264 moonlight ttf-weather-icons
